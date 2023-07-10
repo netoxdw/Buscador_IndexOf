8121708c56bba40beb8193c7876e02e251e79c52
netoxdw <neto.guevara.tapia@gmail.com>
2023-07-09 21:42:11 -0600

    Agregando array de productos

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// Lista de productos
+const productos = [
+    {nombre: 'Platanos', valor: '500'},
+    {nombre: 'Pera', valor: '500'},
+    {nombre: 'Sandia', valor: '500'},
+    {nombre: 'Melon', valor: '500'}
+] 
+
+
+
 // Aqui se almacena lo que el uduario escribe (lo de input)
 const formulario = document.querySelector('#formulario');
 // Aqui se registra el click al boton
