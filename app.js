7638d95d3d35a634527adf3143a5df3fe675d596
netoxdw <neto.guevara.tapia@gmail.com>
2023-07-09 22:08:32 -0600

    Terminando proyecto

--- app.js
+++ app.js
@@ -12,20 +12,34 @@ const productos = [
 const formulario = document.querySelector('#formulario');
 // Aqui se registra el click al boton
 const boton = document.querySelector('#boton');
+// Constante de resultado
+const resultado = document.querySelector('#resultado')
 
 // Funcion donde filtra a consola lo que el usuario busca
 const filtrar = () => {
     // console.log(formulario.value);
+
+    resultado.innerHTML = '';
     // Lo que el usuario escribe pasar a minusculas tolowerCase
-    const texto = formulario.ariaValueMax.toLowerCase()
+    const texto = formulario.value.toLowerCase()
     // recorrer todo el array productos
     for(let i of productos){
         let nombre = i.nombre.toLowerCase();
         if (nombre.indexOf(texto) !== -1){
+            resultado.innerHTML += `
+            <li>${i.nombre} -Valor: ${i.valor}</li> 
+            `
             
         }
+    }
+    
+    if (resultado.innerHTML === ''){
+        resultado.innerHTML += `
+        <li>Producto no encontrado</li> 
+        `
     }
 }
 
 // Evento al dar click buscar, se activa la funcion filtrar
 boton.addEventListener('click', filtrar)
+formulario.addEventListener('keyup', filtrar)
